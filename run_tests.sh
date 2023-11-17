8150cb5cf71aa4403a9818edb62352cb5c9ee5e4
rvarun11 <rvarun011@gmail.com>
2023-11-17 22:25:49 +0000

    add test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo -e "\e[1mRunning unit and integration tests. Note: Some integration tests may take longer to execute, so please be patient.\e[0m"
+echo -e "\e[1mTest Information:\e[0m The test suite focuses exclusively on critical functionalities essential for key scenarios."
+echo -e "\e[1mRunning unit and integration tests. Note: Some integration tests may have longer execution times, so your patience is appreciated.\e[0m"
+echo -e "\n"
 
 for dir in */; do
     cd $dir
-    echo "Running tests in $dir.."
+    echo -e "\e[1mRunning tests in $dir\e[0m"
     go test -v ./...
     cd ..
+    echo -e "\n"
 done
