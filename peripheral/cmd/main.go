0d834bd24a564fc45a3e15b81d70463629228484
Liuyin Shi <cobbshi810@outlook.com>
2023-11-24 00:12:06 +0000

    migrated trail and peripheral together

--- peripheral/cmd/main.go
+++ peripheral/cmd/main.go
@@ -5,7 +5,8 @@ import (
 	"os"
 
 	"github.com/CAS735-F23/macrun-teamvsl/peripheral/config"
-	"github.com/CAS735-F23/macrun-teamvsl/peripheral/internal/adapters/handler"
+	httphandler "github.com/CAS735-F23/macrun-teamvsl/peripheral/internal/adapters/handler/http"
+	rabbitmqhandler "github.com/CAS735-F23/macrun-teamvsl/peripheral/internal/adapters/handler/rabbitmq"
 	"github.com/CAS735-F23/macrun-teamvsl/peripheral/internal/adapters/repository"
 	"github.com/CAS735-F23/macrun-teamvsl/peripheral/internal/core/services"
 	log "github.com/CAS735-F23/macrun-teamvsl/peripheral/log"
@@ -24,15 +25,19 @@ func main() {
 	// Initialize the repository
 	repo := repository.NewMemoryRepository()
 
-	// Initialize the Peripheral service
-	peripheralService := services.NewPeripheralService(repo)
-
-	// Set up the RabbitMQ connection string
 	amqpURL := "amqp://" + cfg.RabbitMQ.User + ":" +
 		cfg.RabbitMQ.Password + "@" + cfg.RabbitMQ.Host + ":" + cfg.RabbitMQ.Port + "/"
-
 	// Initialize the RabbitMQ handler with the Peripheral service and the AMQP URL
-	peripheralAMQPHandler, err1 := handler.NewRabbitMQHandler(peripheralService, amqpURL) // Adjusted for package
+
+	peripheralAMQPHandler, err1 := rabbitmqhandler.NewRabbitMQHandler(amqpURL) // Adjusted for package
+
+	// Initialize rabbitmqhandler
+
+	// Initialize the Peripheral service
+	peripheralService := services.NewPeripheralService(repo, peripheralAMQPHandler)
+
+	// Set up the RabbitMQ connection string
+
 	if err1 != nil {
 		// log.Fatal("Error setting up RabbitMQ %v ", zap.error(err1))
 		fmt.Fprintf(os.Stderr, "Error setting up RabbitMQ: %v\n", err1)
@@ -40,7 +45,7 @@ func main() {
 	defer peripheralAMQPHandler.Close()
 
 	// Initialize the HTTP handler with the Peripheral service and the RabbitMQ handler
-	peripheralHTTPHandler := handler.NewPeripheralServiceHTTPHandler(router, peripheralService, peripheralAMQPHandler) // Adjusted for package
+	peripheralHTTPHandler := httphandler.NewPeripheralServiceHTTPHandler(router, peripheralService, peripheralAMQPHandler) // Adjusted for package
 
 	// Set up the HTTP routes
 	peripheralHTTPHandler.InitRouter()
@@ -51,3 +56,81 @@ func main() {
 		// log.Fatal("Failed to run the server: %v", err)
 	}
 }
+
+// package main
+
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"os"
+// 	"sync"
+
+// 	"github.com/CAS735-F23/macrun-teamvsl/hrm/internal/adapters/handler"
+// 	"github.com/CAS735-F23/macrun-teamvsl/hrm/internal/adapters/repository"
+// 	"github.com/CAS735-F23/macrun-teamvsl/hrm/internal/core/services"
+// 	"github.com/gin-gonic/gin"
+// )
+
+// var (
+// 	repo = flag.String("db", "postgres", "Database for storing messages")
+// 	//    redisHost   = "localhost:6379"
+// 	//    httpHandler *handler.HTTPHandler
+// 	svc *services.HRMService
+// )
+
+// func main() {
+// 	// flag.Parse()
+
+// 	fmt.Printf("Application running using %s\n", *repo)
+// 	switch *repo {
+// 	// note: we can have other repositories like redis, mysql, etc
+// 	//    case "redis":
+// 	//        store := repository.NewMessengerRedisRepository(redisHost)
+// 	//        svc = services.NewMessengerService(store)
+// 	default:
+// 		store := repository.NewMemoryRepository()
+// 		svc = services.NewHRMService(store)
+// 	}
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
+
+// 	go InitRabbitMQ(&wg)
+// 	go svc.SendHRM(&wg)
+// 	InitRoutes()
+
+// 	wg.Wait()
+// }
+
+// func InitRabbitMQ(wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	cfg := NewConfig()
+// 	handler.HRMWorkoutBinder(*svc, cfg.RABBITMQ_URL)
+// }
+
+// func InitRoutes() {
+// 	router := gin.Default()
+// 	handler := handler.NewHTTPHandler(*svc)
+// 	router.POST("/hrms", handler.ConnectHRM)
+// 	// TODO: Implement when needed
+// 	// router.PUT("/player", handler.UpdatePlayer)
+// 	router.Run(":8004")
+
+// }
+
+// // TODO: Handle service configurations properly
+// type Config struct {
+// 	RABBITMQ_URL string
+// }
+
+// func NewConfig() Config {
+// 	return Config{
+// 		RABBITMQ_URL: getEnv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/"),
+// 	}
+// }
+
+// func getEnv(key, defaultValue string) string {
+// 	if value, exists := os.LookupEnv(key); exists {
+// 		return value
+// 	}
+// 	return defaultValue
+// }
