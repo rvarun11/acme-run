8b86a75a9cfed98e362f7ea1ad50a0fde70cc677
Liuyin Shi <cobbshi810@outlook.com>
2023-12-02 02:49:13 +0000

    moved the printDB and initializeDB from main.go to notes.txt

--- zone/cmd/main.go
+++ zone/cmd/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
 	"github.com/CAS735-F23/macrun-teamvsl/zone/config"
 	httphandler "github.com/CAS735-F23/macrun-teamvsl/zone/internal/adapters/primary/http"
 	"github.com/CAS735-F23/macrun-teamvsl/zone/internal/adapters/primary/peripheralhandler"
@@ -19,38 +16,6 @@ import (
 
 var cfg *config.AppConfiguration = config.Config
 
-// ensureDatabaseExists checks for the existence of the database and creates it if it doesn't exist
-func ensureDatabaseExists(cfg *config.Postgres) error {
-	// Connection string without the database name
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable",
-		cfg.Host, cfg.Port, cfg.User, cfg.Password)
-
-	// Open a connection to the database server
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	// Check if the database exists
-	var exists bool
-	query := `SELECT EXISTS(SELECT 1 FROM pg_database WHERE datname=$1);`
-	err = db.QueryRow(query, cfg.DB_Name).Scan(&exists)
-	if err != nil {
-		return err
-	}
-
-	// If the database does not exist, create it
-	if !exists {
-		_, err = db.Exec(fmt.Sprintf("CREATE DATABASE %s;", cfg.DB_Name))
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 func main() {
 	log.Info("Trail Service is starting")
 
@@ -59,8 +24,6 @@ func main() {
 	router.Use(gin.Recovery())
 
 	// initialize a database for test purposes
-	// initializeDB()
-	err := ensureDatabaseExists(cfg.Postgres)
 
 	// Initialize the repository
 	repo := repository.NewMemoryRepository()
@@ -83,7 +46,7 @@ func main() {
 	phandler.InitAMQP()
 
 	// Start the HTTP server
-	err = router.Run(":" + cfg.Port)
+	err := router.Run(":" + cfg.Port)
 	if err != nil {
 		log.Fatal("Failed to run the server", zap.Error(err))
 	}
