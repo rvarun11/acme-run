f331c6f6aea4a359f28723881cc7e72a7d8692c7
rvarun11 <rvarun011@gmail.com>
2023-12-04 15:17:59 -0500

    deployyyy

--- challenge/run_tests.sh
+++ challenge/run_tests.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 echo -e "\e[1mWelcome to Challenge Manager Test Suite.\e[0m"
-echo "The test suite only focuses on critical functionalities for key scenarios. It includes both unit and integration tests."
-echo "Some integration tests may take longer; your patience is appreciated."
+echo "The test suite only focuses on critical functionalities for key scenarios."
+echo "Some tests may take longer; your patience is appreciated."
 echo "Running tests..."
 
-echo -e "\n"
 go test -v ./...
-echo -e "\n"
 
 echo -e "\e[1mChallenge Manager Test Suite has completed."
--- user/run_tests.sh
+++ user/run_tests.sh
@@ -5,8 +5,6 @@ echo "The test suite only focuses on critical functionalities for key scenarios.
 echo "Some tests may take longer; your patience is appreciated."
 echo "Running tests..."
 
-echo -e "\n"
 go test -v ./...
-echo -e "\n"
 
 echo -e "\e[1mUser Manager Test Suite has completed."
